f7dd316dafb59a6be9237007e64c548aebccf909
Mustafa-chow <chowdhury.mustaf@gmail.com>
2019-01-29 19:31:44 -0500

    HTML website

---
+++ Photo_contest/js/header.js
@@ -0,0 +1,25 @@
+function header() 
+{
+	document.write("<div id='header'>");
+	document.write("<img src='js/header.jpg' height='310' width='980' id='headerimage' alt='Art and Photography Club'/>");
+	document.write("</div>");
+	document.write("<div id='navigation'>");
+	document.write("<div class='horizontalcssmenu'>");
+	document.write("<ul id='cssmenu1'>");
+	document.write("<li><a href='index.html'>Home</a></li>");
+	document.write("<li><a href='rules.html'>Rules</a></li>");
+        document.write("<li><a href='topics.html'>Topics</a></li>");
+	document.write("<li><a href='archive.html'>Galleries</a>");
+	document.write("</li>");
+	document.write("<li><a href='vote.html'>vote</a>");
+	document.write("<ul>");
+	document.write("<li><a href='still-life.html'>Vote for still life catagory</a></li>");
+	document.write("<li><a href='sports.html'>Votes for sports catagory</a></li>");
+	document.write("<li><a href='foods.html'>Vote for  foods catagory</a></li>");
+	document.write("</ul>");
+	document.write("<li><a href='contact.html'>Contact</a></li>");
+	document.write("</ul>");
+	document.write("<div id='clockbox' style='color:#fff;padding-right:10px;text-align:right;'></div>");
+	document.write("</div>");
+	document.write("</div>");
+} 
